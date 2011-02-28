47cd62683dd560a2305067e16bb379be34035590
Rodrigo Vanegas <rvanegas@gmail.com>
2011-02-27 23:25:15 -0800

    added read and renamed all methods

--- lib/model_cache.rb
+++ lib/model_cache.rb
@@ -3,58 +3,64 @@ module ActiveRecord
     def self.included(base)
       base.extend(ClassMethods)
     end
-    def model_cache_keys
-      self.class.indeces.map do |index|
+    def mcache_keys
+      self.class.mcache_indeces.map do |index|
         pairs = index.map do |name| 
           { name => self[name] }
         end
-        self.class.model_cache_key pairs.inject :merge
+        self.class.mcache_key pairs.inject :merge
       end
     end
-    def write_to_cache
-      model_cache_keys.each do |key|
+    def mcache_write
+      mcache_keys.each do |key|
         Rails.cache.write(key, {"id" => id, "attributes" => attributes})
       end
     end
-    def delete_from_cache
-      model_cache_keys.each do |key|
+    def mcache_delete
+      mcache_keys.each do |key|
         Rails.cache.delete(key)
       end
     end
     module ClassMethods
-      mattr_accessor :indeces
-      def model_cache_key(attrs)
+      mattr_accessor :mcache_indeces
+      def mcache_key(attrs)
         attrs = attrs.stringify_keys
         index = attrs.keys.sort
-        raise ArgumentError, "index not available" unless self.indeces.include? index
+        raise ArgumentError, "index not available" unless self.mcache_indeces.include? index
         key = index.map{|name| [name, attrs[name]]}
         key << self.name
         key << "ModelCache.v1"
         return key.to_json
       end
-      def find_from_model_cache(attrs)
-        key = model_cache_key(attrs)
+      def mcache_find(attrs)
+        unless obj = mcache_read(attrs)
+          obj = where(attrs).first
+          obj.mcache_write if obj
+        end
+        return obj
+      end
+      def mcache_read(attrs)
+        key = mcache_key(attrs)
         if val = Rails.cache.read(key)
           obj = new(val["attributes"])
           obj.id = val["id"]
           obj.instance_variable_set("@new_record", false)
         else
-          obj = where(attrs).first
-          obj.write_to_cache if obj
+          obj = nil
         end
         return obj
       end
-      def add_to_indeces(*index)
-        self.indeces ||= [["id"]]
+      def mcache_add_index(*index)
+        self.mcache_indeces ||= [["id"]]
         if index.present?
           index = index.map{|n|n.to_s}.sort
           unless index.all?{|n|self.column_names.include? n}
             raise ArgumentError, "index must be array of column names" 
           end
-          self.indeces.push index unless self.indeces.include? index
+          self.mcache_indeces.push index unless self.mcache_indeces.include? index
         end
-        self.after_save :write_to_cache
-        self.after_destroy :delete_from_cache
+        self.after_save :mcache_write
+        self.after_destroy :mcache_delete
         return nil
       end
     end
@@ -62,7 +68,7 @@ module ActiveRecord
   class Base
     def self.model_cache(*index)
       include ModelCache
-      add_to_indeces(*index)
+      mcache_add_index(*index)
     end
   end
 end
